c0b9548cbc4e54dccf67caa963f4830418b22705
Paul Sherman <paul.sherman.88@gmail.com>
2017-12-12 14:14:37 -0600

    Test Home.vue
    I'm not sure whether or not I want to write these tests, but I might as well get them working

---
+++ test/unit/specs/Home.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import Home from '@/components/Home';
+
+describe('Home.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(Home);
+    const vm = new Constructor().$mount();
+    const h1 = vm.$el.querySelector('h1');
+    expect(h1.textContent).toEqual('Home page');
+  });
+});
