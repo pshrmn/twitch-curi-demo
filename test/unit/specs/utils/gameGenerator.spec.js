54477e0cfadd2e54cf8d5e946c8827db31fca59a
Paul Sherman <paul.sherman.88@gmail.com>
2017-12-14 21:07:30 -0600

    Fix gameGenerator test

--- test/unit/specs/utils/gameGenerator.spec.js
+++ test/unit/specs/utils/gameGenerator.spec.js
@@ -1,9 +1,19 @@
 import { game } from '@/utils/gameGenerator'
 
 describe('game generator', () => {
-  it('returns a string with three words', () => {
+  it('returns a game object', () => {
     const g = game();
-    expect(typeof g).toBe('string');
-    expect(g.split(' ').length).toBe(3);
+    expect(g).toMatchObject(
+      expect.objectContaining({
+        name: expect.any(String),
+        watching: 0,
+        colors: expect.objectContaining({
+          top: expect.any(String),
+          right: expect.any(String),
+          bottom: expect.any(String),
+          left: expect.any(String)
+        })
+      })
+    )
   });
 });
